c32307ecf5997e357a1e86e98fe3fc2dbde4ee57
arsam <arsamrahmaan31@gmail.com>
2017-10-03 09:58:34 -0400

    added change action and custom route

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy, :change]
 
   # GET /tasks
   # GET /tasks.json
@@ -64,14 +64,22 @@ class TasksController < ApplicationController
     end
   end
 
-  private
-  # Use callbacks to share common setup or constraints between actions.
-  def set_task
-    @task = Task.find(params[:id])
-  end
-
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def task_params
-    params.require(:task).permit(:content, :state)
+  def change
+    @task.update_attributes(state: params[:state])
+    respond_to do |format|
+      format.html {redirect_to tasks_path, notice: "task update"}
+    end
   end
 end
+
+private
+# Use callbacks to share common setup or constraints between actions.
+def set_task
+  @task = Task.find(params[:id])
+end
+
+# Never trust parameters from the scary internet, only allow the white list through.
+def task_params
+  params.require(:task).permit(:content, :state)
+end
+
